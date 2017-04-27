f6c0ef0b6160505384f9d316c3862380582dcebf
mryoshio <yoshiokaas@gmail.com>
2017-04-27 21:50:12 +0900

    dup3
    - create printCount function

--- c1.go
+++ c1.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io/ioutil"
 	"os"
 	"strings"
 )
@@ -19,13 +20,15 @@ func p9_2() {
 	fmt.Println()
 }
 
-func p12() {
+func dup2() {
 	// go run c1.go < dummy.txt
 	counts := make(map[string]int)
 	files := os.Args[1:]
 	if len(files) == 0 {
+		fmt.Println("From stdin")
 		countLines(os.Stdin, counts)
 	} else {
+		fmt.Println("From arg")
 		for _, arg := range files {
 			f, err := os.Open(arg)
 			if err != nil {
@@ -36,11 +39,22 @@ func p12() {
 			f.Close()
 		}
 	}
-	for line, n := range counts {
-		if n > 1 {
-			fmt.Printf("%d\t%s\n", n, line)
+	printCount(counts)
+}
+
+func dup3() {
+	counts := make(map[string]int)
+	for _, filename := range os.Args[1:] {
+		data, err := ioutil.ReadFile(filename)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "dup3: %v\n", err)
+			continue
+		}
+		for _, line := range strings.Split(string(data), "\n") {
+			counts[line]++
 		}
 	}
+	printCount(counts)
 }
 
 func countLines(f *os.File, counts map[string]int) {
@@ -50,8 +64,17 @@ func countLines(f *os.File, counts map[string]int) {
 	}
 }
 
+func printCount(counts map[string]int) {
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
+}
+
 func main() {
 	//p9_1()
 	//p9_2()
-	p12()
+	//dup2()
+	dup3()
 }
