af23bfdf542e51102b36a2e19be8eec2cbd26bd0
mryoshio <yoshiokaas@gmail.com>
2017-04-27 21:41:32 +0900

    dup2

--- c1.go
+++ c1.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 	"strings"
@@ -18,8 +19,39 @@ func p9_2() {
 	fmt.Println()
 }
 
+func p12() {
+	// go run c1.go < dummy.txt
+	counts := make(map[string]int)
+	files := os.Args[1:]
+	if len(files) == 0 {
+		countLines(os.Stdin, counts)
+	} else {
+		for _, arg := range files {
+			f, err := os.Open(arg)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
+				continue
+			}
+			countLines(f, counts)
+			f.Close()
+		}
+	}
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
+}
+
+func countLines(f *os.File, counts map[string]int) {
+	input := bufio.NewScanner(f)
+	for input.Scan() {
+		counts[input.Text()]++
+	}
+}
+
 func main() {
-	p9_1()
-	p9_2()
-	//	p9_3()
+	//p9_1()
+	//p9_2()
+	p12()
 }
