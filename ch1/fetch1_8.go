43218156b91da8cc4c28575e6f8b8993ff41e8d1
mryoshio <yoshiokaas@gmail.com>
2017-05-04 21:53:53 +0900

    add scheme when not specified

---
+++ ch1/fetch1_8.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		if !strings.HasPrefix(url, "http://") {
+			url = fmt.Sprintf("%s%s", "http://", url)
+		}
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch error: %v\n", err)
+			os.Exit(1)
+		}
+		if _, err = io.Copy(os.Stdout, resp.Body); err != nil {
+			fmt.Fprintf(os.Stderr, "copy error: %v\n", err)
+		}
+		resp.Body.Close()
+	}
+}
